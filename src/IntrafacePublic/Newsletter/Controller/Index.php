5182749a01a5f902de865d8e2ced703f44a3ca11
Lars Olesen <lars@intraface.dk>
2010-10-07 16:39:10 +0200

    Converted to konstrukt 2

--- src/IntrafacePublic/Newsletter/Controller/Index.php
+++ src/IntrafacePublic/Newsletter/Controller/Index.php
@@ -4,10 +4,12 @@ require_once 'get_ip_address.php';
 class IntrafacePublic_Newsletter_Controller_Index extends k_Component
 {
     protected $template;
+    protected $client;
 
-    function __construct(k_TemplateFactory $template)
+    function __construct(IntrafacePublic_Newsletter_Client_XMLRPC $client, k_TemplateFactory $template)
     {
         $this->template = $template;
+        $this->client = $client;
     }
 
     public function renderHtml()
@@ -25,22 +27,21 @@ class IntrafacePublic_Newsletter_Controller_Index extends k_Component
         if (!Validate::email($this->body('email'))) {
             $data['message'] = 'An error occured. E-mail is not valid.';
         } else {
-            $client = $this->registry->get('newsletter');
 
             if ($this->body('mode') == 1) {
-                if ($client->subscribe($this->body('email'), $this->body('name'), get_ip_address(''))) {
+                if ($this->client->subscribe($this->body('email'), $this->body('name'), get_ip_address(''))) {
                     $data = array();
                     $data['message'] = 'You have now subscribed to the newsletter.';
                 } else {
                     $data['message'] = 'An error occured. You could not subscribe.';
                 }
             } elseif ($this->body('mode') == 2) {
-                if(!empty($this->body('comment'))) {
+                if ($this->body('comment')) {
                     $comment = $this->body('comment');
                 } else {
                     $comment = '';
                 }
-                if ($client->unsubscribe($this->body('email'), $comment)) {
+                if ($this->client->unsubscribe($this->body('email'), $comment)) {
                     $data = array();
                     $data['message'] = 'You have unsubscribed from the newsletter.';
                 } else {
